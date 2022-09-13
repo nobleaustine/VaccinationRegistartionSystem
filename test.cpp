a548d1843361923d8fc184acd75bd18c01d824b7
OMAL S <95520329+omals@users.noreply.github.com>
2022-09-13 23:14:50 +0530

    added read function

--- test.cpp
+++ test.cpp
@@ -1,12 +1,37 @@
 #include<iostream>
 #include<fstream>
+#include <cstring>
+
 using namespace std;
 int main()
 {
  ofstream myfile;
- myfile.open("test.csv");
- for( int i=0 ; i<20; i++)
- {
-    myfile << i << "," << i*i << endl;
+ myfile.open("test.csv",ofstream::app);
+ int inp=1;
+ while(inp==1){
+    int id;
+    string name;
+    cout<<"name\n";
+    cin>>name;
+    cout<<"id\n";
+    cin>>id;
+
+    cout<<"ctnu?\n";
+    cin>>inp;
+
+    myfile << id << "," << name << endl;
  }
+//  for( int i=0 ; i<20; i++)
+//  {
+//     myfile << i << "," << i*i << endl;
+//  }
+
+ifstream myFile;
+myFile.open("test.csv");
+while(myFile.good()) {
+   string line;
+   getline(myFile , line, ',');
+   cout<<line<<endl;
+}
+myFile.close();
 }
